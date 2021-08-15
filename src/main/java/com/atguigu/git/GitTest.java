ccf1b15f2bf7933fec0108993bcd441a7cbaac9a
honglingzhou <88962562+honglingzhou@users.noreply.github.com>
2021-08-15 23:10:21 +0800

    by github

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello ......");
         System.out.println("hello ......by master");
         System.out.println("hello ......by hot-fix");
+        System.out.println("hello ......by github");
     }
 }
